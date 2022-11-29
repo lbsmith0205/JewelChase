f018e710d13006eabad131ed6ef1fa317be77ad0
SheggedStudent <2016944@swansea.ac.uk>
2022-11-29 14:11:58 +0000

    Collaborating with the tiles class to form one tile

--- src/Game/Levels.java
+++ src/Game/Levels.java
@@ -4,12 +4,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import javafx.application.Application;
 import javafx.scene.Group;
-import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import static javafx.application.Application.launch;
 
 public class Levels extends Application {
     private ArrayList<Character> characters = new ArrayList<Character>();
@@ -17,7 +15,13 @@ public class Levels extends Application {
     private ArrayList<Item>Items = new ArrayList<Item>();
     //Arraylist of items in level
     private Tile [] tiles;// Array of tile objects to show board
-    public Levels(){}
+    private Color[] tileColors = {Color.INDIANRED,Color.
+            SPRINGGREEN,Color.DEEPSKYBLUE,Color.KHAKI};;
+    public Levels(){
+    }
+
+    public Color[] getTileColors(){return tileColors;}
+
 
     public static void main(String[] args){
         launch(args);
@@ -26,11 +30,43 @@ public class Levels extends Application {
 
     public void start(Stage primaryStage) throws Exception{
 
+        getTileColors();
+
         Group root = new Group();
         Scene scene = new Scene(root, 1080,720);
         Stage stage = new Stage();
         stage.setTitle("Drawing Tests");
 
+        Tile tile = new Tile(540,330,tileColors);
+
+        Rectangle tileVer1 = new Rectangle();
+        tileVer1.setX(tile.getXPosition());
+        tileVer1.setY(tile.getYPosition());
+        tileVer1.setWidth(30);
+        tileVer1.setHeight(30);
+        tileVer1.setFill(tileColors[0]);
+
+        Rectangle tileVer2 = new Rectangle();
+        tileVer2.setX(tile.getXPosition()+30);
+        tileVer2.setY(tile.getYPosition());
+        tileVer2.setWidth(30);
+        tileVer2.setHeight(30);
+        tileVer2.setFill(tileColors[1]);
+
+        Rectangle tileVer3 = new Rectangle();
+        tileVer3.setX(tile.getXPosition());
+        tileVer3.setY(tile.getYPosition()+30);
+        tileVer3.setWidth(30);
+        tileVer3.setHeight(30);
+        tileVer3.setFill(tileColors[2]);
+
+        Rectangle tileVer4 = new Rectangle();
+        tileVer4.setX(tile.getXPosition()+30);
+        tileVer4.setY(tile.getYPosition()+30);
+        tileVer4.setWidth(30);
+        tileVer4.setHeight(30);
+        tileVer4.setFill(tileColors[3]);
+/*
         Rectangle tileTest = new Rectangle();
         tileTest.setX(505);
         tileTest.setY(330);
@@ -63,8 +99,11 @@ public class Levels extends Application {
         root.getChildren().add(tileTest2);
         root.getChildren().add(tileTest3);
         root.getChildren().add(tileTest4);
-
-
+*/
+        root.getChildren().add(tileVer1);
+        root.getChildren().add(tileVer2);
+        root.getChildren().add(tileVer3);
+        root.getChildren().add(tileVer4);
         stage.setScene(scene);
         stage.show();
     }
