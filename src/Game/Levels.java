b829b6ac51fd2d2fc5cbbd7b45a4cc9adf2788f2
SheggedStudent <2016944@swansea.ac.uk>
2022-11-29 12:07:21 +0000

    importing necessary javafx packages

--- src/Game/Levels.java
+++ src/Game/Levels.java
@@ -2,23 +2,20 @@ package Game;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class Levels {
     private ArrayList<Character> characters = new ArrayList<Character>();
-    //private ArrayList<Item>Items = new ArrayList<Item>();
-    private Tile [] tiles;
-
-    private static ArrayList<Character> readLineByLine(Scanner in) {
-        ArrayListy<ClosedShape> shapeQueue = new ArrayList<ClosedShape>();
-        while (in.hasNextLine()) {
-            String curLine = in.nextLine();
-            Scanner line = new Scanner(curLine);
-            String type = line.next();
-
-        }
-    }
-
-
+    //Arraylist of characters
+    private ArrayList<Item>Items = new ArrayList<Item>();
+    //Arraylist of items in level
+    private Tile [] tiles;// Array of tile objects to show board
     public Levels(){}
+
+
 
 }
