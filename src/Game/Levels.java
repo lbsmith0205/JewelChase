affa6ba034b235ec99babb5262e7c3c15f10fdb2
SheggedStudent <2016944@swansea.ac.uk>
2022-11-29 12:25:00 +0000

    Run a javafx window

--- src/Game/Levels.java
+++ src/Game/Levels.java
@@ -3,18 +3,35 @@ package Game;
 import java.util.ArrayList;
 import java.util.Scanner;
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import static javafx.application.Application.launch;
 
-public class Levels {
+public class Levels extends Application {
     private ArrayList<Character> characters = new ArrayList<Character>();
     //Arraylist of characters
     private ArrayList<Item>Items = new ArrayList<Item>();
     //Arraylist of items in level
     private Tile [] tiles;// Array of tile objects to show board
     public Levels(){}
+
+    public static void main(String[] args){
+        launch(args);
+
+    }
+
+    public void start(Stage primaryStage) throws Exception{
+
+        Group root = new Group();
+        Scene scene = new Scene(root, 600,600);
+        Stage stage = new Stage();
+
+        stage.setScene(scene);
+        stage.show();
+    }
 
 
 
