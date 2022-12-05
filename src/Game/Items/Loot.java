b0df18724f20d7033a382cee352c8cd7810c00b6
Nguyen-CK <caokhoinguyen2603@gmail.com>
2022-12-05 20:14:19 +0000

    Create LootType

--- src/Game/Items/Loot.java
+++ src/Game/Items/Loot.java
@@ -11,6 +11,8 @@ public class Loot extends Item{
     private static final int RUBY_VALUE = 5;
     private static final int DIAMOND_VALUE = 10;
 
+    private final LootType type;
+
     private int value;
 
     //need to change the way to construct a Loot
@@ -19,15 +21,22 @@ public class Loot extends Item{
         switch (lootType){
             case "¢":
                 value = CENT_COIN_VALUE;
+                this.type = LootType.CENT;
                 break;
             case "$":
                 value = DOLLAR_COIN_VALUE;
+                this.type = LootType.DOLLAR;
                 break;
             case "Ru":
                 value = RUBY_VALUE;
+                this.type = LootType.RUBY;
                 break;
             case "Di":
                 value = DIAMOND_VALUE;
+                this.type = LootType.DIAMOND;
+                break;
+            default:
+                type = null;
                 break;
         }
 
@@ -41,6 +50,8 @@ public class Loot extends Item{
     public void setValue(int value) {
         this.value = value;
     }
+
+    public LootType getType() { return this.type;}
 
     public void interact(Character c) {
         //score.add(this.value);
