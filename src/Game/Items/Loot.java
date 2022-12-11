449bb51e73bd9d2f44c5d562bee767a0302843b9
DBaxter991645 <991645@swansea.ac.uk>
2022-12-11 13:24:07 +0000

    Adjusted Loot.

--- src/Game/Items/Loot.java
+++ src/Game/Items/Loot.java
@@ -17,7 +17,11 @@ public class Loot extends Item{
     private static final String DOLLAR_SPRITE_PATH = "Sprites/Items/Dollar.png";
     private static final String DIAMOND_SPRITE_PATH = "Sprites/Items/Diamond.png";
     private static final String RUBY_SPRITE_PATH = "Sprites/Items/Ruby.png";
-    private static final String LOOTABLE_DAY_PATH = "Sprites/Items/LootableDoor.png";
+    private static final String LOOTABLE_DOOR_PATH = "Sprites/Items/LootableDoor.png";
+
+    private static final String RED_LEVER_PATH = "Sprites/Items/Levers/LeverRed.png";
+    private static final String GREEN_LEVER_PATH = "Sprites/Items/Levers/LeverGreen.png";
+    private static final String BLUE_LEVER_PATH = "Sprites/Items/Levers/LeverBlue.png";
     private static final int CENT_COIN_VALUE = 1;
     private static final int DOLLAR_COIN_VALUE = 2;
     private static final int RUBY_VALUE = 5;
@@ -51,7 +55,19 @@ public class Loot extends Item{
             }
             case "LD" -> {
                 this.value = 0;
-                this.lootSprite = LOOTABLE_DAY_PATH;
+                this.lootSprite = LOOTABLE_DOOR_PATH;
+            }
+            case "RL" -> {
+                this.value = 0;
+                this.lootSprite = RED_LEVER_PATH;
+            }
+            case "GL" -> {
+                this.value = 0;
+                this.lootSprite = GREEN_LEVER_PATH;
+            }
+            case "BL" -> {
+                this.value = 0;
+                this.lootSprite = BLUE_LEVER_PATH;
             }
             default -> {
                 this.lootType = null;
